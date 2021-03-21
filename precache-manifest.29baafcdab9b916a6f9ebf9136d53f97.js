c307defd19f0844620f6e21cd28d9afaaa4fbf93
Melo <ameliosconnection@gmail.com>
2021-03-21 07:35:33 +0200

    Updates

---
+++ precache-manifest.29baafcdab9b916a6f9ebf9136d53f97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf3fae4c4cc73b0457fd7663106e39c",
+    "url": "/react-algo/index.html"
+  },
+  {
+    "revision": "6d4f35ed80ff3b03e5c3",
+    "url": "/react-algo/static/css/main.348a01c8.chunk.css"
+  },
+  {
+    "revision": "fddefbd540edb8074e5f",
+    "url": "/react-algo/static/js/2.289384a4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-algo/static/js/2.289384a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4f35ed80ff3b03e5c3",
+    "url": "/react-algo/static/js/main.abc0bf06.chunk.js"
+  },
+  {
+    "revision": "3886863eabcd0e4194c0",
+    "url": "/react-algo/static/js/runtime-main.4fdab4ae.js"
+  },
+  {
+    "revision": "e5debc15b363fa6d3599bcbf836fae3c",
+    "url": "/react-algo/static/media/react-algo2.e5debc15.png"
+  },
+  {
+    "revision": "27181354977486525464e16d1efd1c84",
+    "url": "/react-algo/static/media/stackll.27181354.png"
+  }
+]);
